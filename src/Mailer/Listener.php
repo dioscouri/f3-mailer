a23582744b200deb2bee0b456a24a85344f71e36
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-01-25 10:58:25 -0500

    prioritized admin nav, refs dioscouri/f3-admin#24

--- src/Mailer/Listener.php
+++ src/Mailer/Listener.php
@@ -8,10 +8,11 @@ class Listener extends \Prefab
         if ($mapper = $event->getArgument('mapper')) 
         {
         	$mapper->reset();
+        	$mapper->priority = 50;
             $mapper->id = 'fa-mailer';
         	$mapper->title = 'Emails';
         	$mapper->route = '';
-        	$mapper->icon = 'fa fa-email';
+        	$mapper->icon = 'fa fa-envelope';
         	$mapper->children = array(
         			json_decode(json_encode(array( 'title'=>'List', 'route'=>'/admin/emails', 'icon'=>'fa fa-list' )))
         			,json_decode(json_encode(array( 'title'=>'Add New User', 'route'=>'/admin/email/create', 'icon'=>'fa fa-plus' )))
