9f168ec97b3eb139d9b0792f2bbd9cd84c38b76b
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-22 10:00:11 -0400

    displaying the list of emails in the admin with a data-dump on detail pages

--- src/Mailer/Admin/Controllers/Email.php
+++ src/Mailer/Admin/Controllers/Email.php
@@ -3,10 +3,51 @@ namespace Mailer\Admin\Controllers;
 
 class Email extends \Admin\Controllers\BaseAuth 
 {
-    public function read()
+    use \Dsc\Traits\Controllers\CrudItemCollection;
+    
+    protected $list_route = '/admin/mailer/emails';
+    protected $create_item_route = '/admin/mailer/email/create';
+    protected $get_item_route = '/admin/mailer/email/read/{id}';
+    protected $edit_item_route = '/admin/mailer/email/edit/{id}';
+    
+    protected function getModel()
     {
         $model = new \Mailer\Models\Emails;
-                
+        return $model;
+    }
+    
+    protected function getItem()
+    {
+        $f3 = \Base::instance();
+        $id = $this->inputfilter->clean( $f3->get('PARAMS.id'), 'alnum' );
+        $model = $this->getModel()
+        ->setState('filter.id', $id);
+    
+        try {
+            $item = $model->getItem();
+        } catch ( \Exception $e ) {
+            \Dsc\System::instance()->addMessage( "Invalid Item: " . $e->getMessage(), 'error');
+            $f3->reroute( $this->list_route );
+            return;
+        }
+    
+        return $item;
+    }
+
+    protected function displayCreate()
+    {
+        $view = \Dsc\System::instance()->get('theme');
+        echo $view->render('Mailer\Admin\Views::emails/read.php');
+    }
+    
+    protected function displayEdit()
+    {
+        $view = \Dsc\System::instance()->get('theme');
+        echo $view->render('Mailer\Admin\Views::emails/read.php');
+    }
+    
+    protected function displayRead()
+    {
         $view = \Dsc\System::instance()->get('theme');
         echo $view->render('Mailer\Admin\Views::emails/read.php');
     }
