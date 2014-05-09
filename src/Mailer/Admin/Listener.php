ccc00080adb5e17994372da52132129438f814e3
Lukas Polak <lpolak@dioscouri.com>
2014-05-08 23:32:40 -0400

    Moved admin menu to mongo

--- src/Mailer/Admin/Listener.php
+++ src/Mailer/Admin/Listener.php
@@ -5,20 +5,29 @@ class Listener extends \Prefab
 {
     public function onSystemRebuildMenu( $event )
     {
-        if ($mapper = $event->getArgument('mapper')) 
-        {
-        	$mapper->reset();
-        	$mapper->priority = 50;
-        	$mapper->title = 'Emails';
-        	$mapper->base = '/admin/mailer';
-        	$mapper->route = '';
-        	$mapper->icon = 'fa fa-envelope';
-        	$mapper->children = array(
-        	        json_decode(json_encode(array( 'title'=>'Dashboard', 'route'=>'/admin/mailer', 'icon'=>'fa fa-dashboard' )))
-        			,json_decode(json_encode(array( 'title'=>'List', 'route'=>'/admin/mailer/emails', 'icon'=>'fa fa-list' )))
-        	        ,json_decode(json_encode(array( 'title'=>'Settings', 'route'=>'/admin/mailer/settings', 'icon'=>'fa fa-cogs' )))
+    	if ($model = $event->getArgument('model'))
+    	{
+    		$root = $event->getArgument( 'root' );
+    		$emails = clone $model;
+    	
+    		$emails->insert(
+    				array(
+    						'type'	=> 'admin.nav',
+    						'priority' => 100,
+    						'title'	=> 'Emails',
+    						'icon'	=> 'fa fa-envelope',
+        					'is_root' => false,
+    						'tree'	=> $root,
+							'base' => '/admin/mailer',
+    				)
+    		);
+    		
+        	$children = array(
+        	        array( 'title'=>'Dashboard', 'route'=>'/admin/mailer', 'icon'=>'fa fa-dashboard' ),
+        			array( 'title'=>'List', 'route'=>'/admin/mailer/emails', 'icon'=>'fa fa-list' ),
+        	        array( 'title'=>'Settings', 'route'=>'/admin/mailer/settings', 'icon'=>'fa fa-cogs' ),
         	);
-        	$mapper->save();
+        	$emails->addChildrenItems( $children, $root, $model );
         	
         	\Dsc\System::instance()->addMessage('Mailer added its admin menu items.');
         }
