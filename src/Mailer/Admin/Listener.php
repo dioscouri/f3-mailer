30d3f738d5658ea28f159a181b13c56f824b153a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-17 10:02:55 -0400

    updated menu item adder

--- src/Mailer/Admin/Listener.php
+++ src/Mailer/Admin/Listener.php
@@ -1,36 +1,46 @@
-<?php 
+<?php
 namespace Mailer\Admin;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
+
+    public function onSystemRebuildMenu($event)
     {
-    	if ($model = $event->getArgument('model'))
-    	{
-    		$root = $event->getArgument( 'root' );
-    		$emails = clone $model;
-    	
-    		$emails->insert(
-    				array(
-    						'type'	=> 'admin.nav',
-    						'priority' => 100,
-    						'title'	=> 'Emails',
-    						'icon'	=> 'fa fa-envelope',
-        					'is_root' => false,
-    						'tree'	=> $root,
-							'base' => '/admin/mailer',
-    				)
-    		);
-    		
-        	$children = array(
-        	        array( 'title'=>'Dashboard', 'route'=>'/admin/mailer', 'icon'=>'fa fa-dashboard' ),
-        			array( 'title'=>'List', 'route'=>'/admin/mailer/emails', 'icon'=>'fa fa-list' ),
-        	        array( 'title'=>'Settings', 'route'=>'/admin/mailer/settings', 'icon'=>'fa fa-cogs' ),
-        	);
-        	$emails->addChildrenItems( $children, $root );
-        	
-        	\Dsc\System::instance()->addMessage('Mailer added its admin menu items.');
+        if ($model = $event->getArgument('model'))
+        {
+            $root = $event->getArgument('root');
+            $emails = clone $model;
+            
+            $emails->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 100,
+                'title' => 'Emails',
+                'icon' => 'fa fa-envelope',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/mailer'
+            ));
+            
+            $children = array(
+                array(
+                    'title' => 'Dashboard',
+                    'route' => '/admin/mailer',
+                    'icon' => 'fa fa-dashboard'
+                ),
+                array(
+                    'title' => 'List',
+                    'route' => '/admin/mailer/emails',
+                    'icon' => 'fa fa-list'
+                ),
+                array(
+                    'title' => 'Settings',
+                    'route' => '/admin/mailer/settings',
+                    'icon' => 'fa fa-cogs'
+                )
+            );
+            $emails->addChildren($children, $root);
+            
+            \Dsc\System::instance()->addMessage('Mailer added its admin menu items.');
         }
-        
     }
 }
