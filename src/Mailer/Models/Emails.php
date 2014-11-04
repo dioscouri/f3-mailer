5593de5837374c05ff431b0f91abbbcad4237a23
Building Berms Server <chris@ammonitenetworks.com>
2014-11-04 04:21:01 +0000

    adding Search Filters to emails model

--- src/Mailer/Models/Emails.php
+++ src/Mailer/Models/Emails.php
@@ -15,4 +15,47 @@ class Emails extends \Dsc\Mongo\Collections\Nodes
             'metadata.created.time' => -1
         ),
     );
+    
+    protected function fetchConditions()
+    {
+    	parent::fetchConditions();
+    
+    $filter_keyword = $this->getState('filter.keyword');
+        if ($filter_keyword&&is_string($filter_keyword))
+        {
+            $key = new \MongoRegex('/'.$filter_keyword.'/i');
+            
+            $where = array();
+            
+            $regex = '/^[0-9a-z]{24}$/';
+            if (preg_match($regex, (string) $filter_keyword))
+            {
+                $where[] = array(
+                    '_id' => new \MongoId((string) $filter_keyword)
+                );
+            }
+            $where[] = array(
+                'Subject' => $key
+            );
+            $where[] = array(
+                'Body' => $key
+            );
+            $where[] = array(
+                'AltBody' => $key
+            );
+           
+            $where[] = array(
+                'metadata.creator.name' => $key
+            );
+            
+            $where[] = array( 
+            		'all_recipients' => $key
+            		
+            );
+            
+            $this->setCondition('$or', $where);
+        }
+    	return $this;
+    }
+    
 }
